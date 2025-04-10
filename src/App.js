fa5b1c7d5a354d248ef78736d33632d74658bc53
massagli <adrielmassagli@gmail.com>
2025-04-09 21:29:08 -0300

    Adding Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './components/pages/Home';
+import Projects from './components/pages/Projects'
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 
 import Container from './layout/Container'
+import Navbar from "./layout/Navbar"
+import Footer from "./layout/Footer"
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo projeto</Link>
-      </div>
+      <Navbar></Navbar>
 
       <Container customClass="min-height">
         <Routes>
           <Route exact path="/" Component={Home}/>
+          <Route exact path="/projects" Component={Projects}/>
           <Route exact path="/contact" Component={Company}/>
-          <Route exact path="/company" Component={Contact}/>
           <Route exact path="/newproject" Component={NewProject}/>
+          <Route exact path="/company" Component={Contact}/>
         </Routes>
       </Container>
       
-      <p>Footer</p>
+      <Footer></Footer>
     </Router>
   );
 }
